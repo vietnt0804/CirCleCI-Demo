b99897975faef2c55269058d4c22bd30d39a9570
vietnt0804 <viet.nguyen@amela.vn>
2024-08-28 17:32:06 +0700

    slot

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <header>
+      <slot name="header">Default Header</slot>
+    </header>
+    <main>
+      <slot>Default Content</slot>
+    </main>
+    <main>
+      <slot>Default Content 1</slot>
+    </main>
+    <footer>
+      <slot name="foo">Default Footer b</slot>
+    </footer>
+    <footer>
+      <slot name="teter">Default Footer a</slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent'
+}
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Card>
+    <template #header>
+      <h1>My Custom Header</h1>
+    </template>
+    <!-- <p>This is the main content of the card.</p> -->
+    <!-- <p>This is the main content of the card.</p> -->
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+    <template #foo>
+      <h1>My Custom Header xxx</h1>
+    </template>
+
+    <!-- <template #defaut>
+      <small>Custom Footer 111</small>
+    </template>
+    <template #oip>
+      <small>Custom Footer</small>
+    </template> -->
+  </Card>
 </template>
+
+<script setup>
+import Card from '@/components/Card.vue'
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
